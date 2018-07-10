217267dc329b884c9a3a6d38f071bad9d813afee
Konstatin <34293528+Konstantin96@users.noreply.github.com>
2018-07-10 20:25:55 +0600

    Добавьте файлы проекта.

---
+++ Tank/Tanks.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tank
+{
+    public class tank
+    {
+        private string name;
+        private int boekomplekt;
+        private int lvlBroni;
+        private int lvlManevr;
+        public tank(int boekomplekt, int lvlBroni, int lvlManevr, string name)
+        {
+
+            this.boekomplekt = boekomplekt;
+            this.lvlBroni = lvlBroni;
+            this.lvlManevr = lvlManevr;
+            this.name = name;
+        }
+        public tank()
+        {
+            boekomplekt = 0;
+            lvlBroni = 0;
+            lvlManevr = 0;
+            name = "null";
+        }
+        public string GetNameTank()
+        {
+            return name;
+        }
+        public void Print()
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("Название танка - " + name+"               ");
+            Console.WriteLine("Боекомплект = " + boekomplekt + " Уровень брони = " + lvlBroni + " Уровень маневренности = " + lvlManevr+ " ");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine();
+        }
+
+        public static tank operator *(tank T34, tank pantera)
+        {
+            if (T34.boekomplekt > pantera.boekomplekt && T34.lvlBroni > pantera.lvlBroni)
+            {
+                return T34;
+            }
+            if (T34.boekomplekt > pantera.boekomplekt && T34.lvlManevr > pantera.lvlManevr)
+            {
+                return T34;
+            }
+            if (T34.lvlBroni > pantera.lvlBroni && T34.lvlManevr > pantera.lvlManevr)
+            {
+                return T34;
+            }
+            else           
+                return pantera;            
+        }
+    }
+}
---
+++ WordOfTanks/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tank;
+
+namespace WordOfTanks
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            tank[] VIN = new tank[5];
+            tank[] T34 = new tank[5];
+
+            for (int i = 0; i < T34.Length; i++)
+            {
+                T34[i] = new tank(rnd.Next(0, 101), rnd.Next(0, 101), rnd.Next(0, 101), "T34");
+            }
+            tank[] pantera = new tank[5];
+            for (int i = 0; i < T34.Length; i++)
+            {
+                pantera[i] = new tank(rnd.Next(0, 101), rnd.Next(0, 101), rnd.Next(0, 101), "Pantera");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.WriteLine("{0} бой\n~~~~~", i+1);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.BackgroundColor = ConsoleColor.Black;
+                T34[i].Print();
+                pantera[i].Print(); 
+                VIN[i] = T34[i] * pantera[i];
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.BackgroundColor = ConsoleColor.DarkMagenta;
+                Console.WriteLine("В {0} схватке победил танк {1}            ",i+1, VIN[i].GetNameTank());
+                Console.WriteLine("******************************** ");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+        }
+    }
+}
